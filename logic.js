3cbd57c636cad9d7a6f6219f36168f49aee94e2a
nBurich <n.burich@hotmail.com>
2019-01-22 20:15:36 -0500

    working NASA

--- logic.js
+++ logic.js
@@ -1,16 +1,34 @@
-let api = "";
-let apiKey = "";
-let query = "";
+var NasaKey = "Df4clu5wZ3LpKIR45I2ZIcl3wuX9P6yOUSeCDXgn";
+
+var queryURL = "https://api.nasa.gov/planetary/apod?api_key=" + NasaKey;
 
 
-function setup () {
-    nocanvas (); {
-        let url = api + apiKey + query;
-        loadJSON(url, gotData);
+$.ajax({
+  url: queryURL,
+  success: function (result) {
+    if ("copyright" in result) {
+
+      console.log(result);
+
+      console.log(queryURL);
+
+      $("#copyright").text("Image Credits: " + result.copyright);
     }
-}
+    else {
+      $("#copyright").text("Image Credits: " + "Public Domain");
+    }
 
-function setup () {
+    if (result.media_type == "video") {
+      $("img").css("display", "none");
+      $("video").attr("src", result.url);
+    }
+    else {
+      $("video").css("display", "none");
+      $("img").attr("src", result.url);
+    }
+    $("#spaceSnippet").text(result.explanation);
+    $("#title").text(result.title);
+    $("#date").text(result.date);
 
-
-}
+  }
+});
